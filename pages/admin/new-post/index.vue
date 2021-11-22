d3c6c9c8770a85221ed9500d67e8d925e6b0ef8e
euclides <euclideskinto@gmail.com>
2021-11-22 17:19:05 -0300

    .

--- pages/admin/new-post/index.vue
+++ pages/admin/new-post/index.vue
@@ -10,7 +10,7 @@
     <div class="container py-5 mx-auto border-b-2">
       <form @submit.prevent="onSave">
         <div class="flex flex-col  my-5">
-            <label for="title" class="text-gray-800 dark:text-gray-100 text-sm font-bold leading-tight tracking-normal mb-2">Titulo</label>
+            <label for="title" class="text-gray-800 dark:text-gray-100 text-sm font-bold leading-tight tracking-normal mb-2">Autor</label>
             <input v-model="editPost.author" autocomplete="off" class="text-gray-600 dark:text-gray-400 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-700 dark:focus:border-indigo-700 dark:border-gray-700 dark:bg-gray-800 bg-white font-normal w-64 h-10 flex items-center pl-3 text-sm border-gray-300 rounded border shadow" />
         </div>
         <div class="flex flex-col  my-5">
@@ -27,6 +27,11 @@
             class=" w-1/4 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-800 text-white bg-indigo-600 cursor-pointer px-3 py-2.5 font-normal text-xs leading-3 shadow-md rounded">
           salvar
         </button>
+            <div
+            class=" w-1/4 mt-3 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-800 text-white bg-red-500 cursor-pointer px-3 py-2.5 font-normal text-xs leading-3 shadow-md rounded text-center"
+            @click="onCancel()">
+          cancelar
+        </div>
         </div>
       </form>
     </div>
@@ -46,10 +51,10 @@
     },
     methods: {
       onSave() {
-        console.log('salvo')
+        console.log(this.editPost)
       },
       onCancel() {
-
+        this.$router.push('/admin');
       }
     },
   }

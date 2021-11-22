3bdbf8ba5d36df5ba335cf7796eb5ad1f1066b50
euclides <euclideskinto@gmail.com>
2021-11-22 14:28:56 -0300

    add pages sobre

---
+++ components/Tutorial.vue
@@ -0,0 +1,4 @@
+<!-- Please remove this file from your project -->
+<template>
+ <h1 class="text-red-700 text-3xl font-bold">Kinto</h1>
+</template>
---
+++ pages/sobre/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1 class="p-5 text-center text-5xl font-semibold">Sobre Nós</h1>
+    <p class="text-center p-6">Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure labore expedita veritatis? Explicabo rerum illo sint sapiente excepturi dicta aperiam similique voluptates reprehenderit, laudantium sunt ex nisi quis praesentium consequuntur?</p>
+  </div>
+
+</template>
+
+<script>
+  export default {
+
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
